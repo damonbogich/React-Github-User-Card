74415a6f5d47b297c2b2886f3f7d763c5e8d927e
damonbogich <damonbogich@gmail.com>
2020-01-14 19:33:38 -0500

    MVP

--- gh-usercard/src/Components/CardList.js
+++ gh-usercard/src/Components/CardList.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import Card from './Card';
+import styled from "styled-components";
+
+const StyledOutterContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;`
+
+const StyledContainer = styled.div`
+border: 2px dashed black;
+width: 33%;
+margin: 5%;`;
 
 const CardList = props => {
     // console.log(props);
     // console.log("here", props.myData);
     return (
         
-        <div>
+        <StyledOutterContainer>
+            <StyledContainer>
                 <Card
                 key = {props.myData.id}
                 name = {props.myData.name}
                 bio = {props.myData.bio}
                 />
-            
-        </div>
+            </StyledContainer>
+        </StyledOutterContainer>
     )
 }
 export default CardList;
--- gh-usercard/src/Components/FollowerCard.js
+++ gh-usercard/src/Components/FollowerCard.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import styled from "styled-components";
+
+const StyledOutterContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;`
+
+const StyledContainer = styled.div`
+border: 2px dashed black;
+width: 33%;
+margin: 5%;`;
 
 //props in this are going to come from CardList
 const FollowerCard = props => {
     console.log('follower name', props.followerName);
 
     return (
-        <div>
-            <p>Username: {props.followerName}</p>
-            <p>Type: {props.followerBio}</p>
-        </div>
+        <StyledOutterContainer>
+            <StyledContainer>
+                <p>Username: {props.followerName}</p>
+                <p>Type: {props.followerBio}</p>
+            </StyledContainer>
+        </StyledOutterContainer>
     )
 }
 
